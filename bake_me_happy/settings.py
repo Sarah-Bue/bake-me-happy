73defdf19567c35347831450740e9eaca2ebb69a
Sarah-Bue <buettner-sarah@gmx.net>
2025-02-04 10:31:33 +0000

    prepare code for deployment

--- bake_me_happy/settings.py
+++ bake_me_happy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0eb@&edbm43@tai$#(t)0l$!yg@^*brz%j3&p(z*%8d6)@8u4u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
