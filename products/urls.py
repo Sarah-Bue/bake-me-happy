efa79e9f9af3ba2c9900334160fcc30cf6cb8e4a
Sarah-Bue <buettner-sarah@gmx.net>
2025-02-11 10:27:25 +0000

    feat: add product detail template

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Product Overview
+    # All Products
     path('', views.all_products, name='products'),
+
+    # Product Detail
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from  .models import Product
 
@@ -7,6 +7,7 @@ def all_products(request):
     """
     A view to show all products, including sorting and search queries
     """
+
     # Get all products
     products = Product.objects.all()
 
@@ -17,4 +18,30 @@ def all_products(request):
     }
 
     # Render the products template with the products
-    return render(request, 'products/products.html', context)
+    return render(
+        request,
+        'products/products.html',
+        context
+    )
+
+
+def product_detail(request, product_id):
+    """
+    A view to show individual product details
+    """
+
+    # Get product with given id or return 404 error
+    product = get_object_or_404(Product, pk=product_id)
+
+    # Context to pass to the template
+    context = {
+        'product': product,
+        'MEDIA_URL': settings.MEDIA_URL
+    }
+
+    # Render the product_detail template with the product
+    return render(
+        request,
+        'products/product_detail.html',
+        context
+    )
