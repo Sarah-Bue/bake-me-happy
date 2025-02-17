5400ac6fbda883d81287209da502526bf8be37e2
Sarah-Bue <buettner-sarah@gmx.net>
2025-02-17 16:08:24 +0000

    feat: add basic checkout functionality and styling

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,92 @@
+/*
+    Core logic/payment flow has been adapted from:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    CSS has been adapted from: 
+    https://stripe.com/docs/stripe-js
+*/
+
+// Get Stripe public key & client_secret from the template - removes quotes from beginning and end
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+
+// Initialize Stripe.js with public key
+var stripe = Stripe(stripePublicKey);
+// Create an instance of Elements
+var elements = stripe.elements();
+
+// Style configuration for the card element
+var style = {
+    base: {
+        // Default styling for the card element
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        // Styling for the placeholder text
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        // Styling for when the card details are invalid
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+
+// Create a card Element and mount it to the card-element div
+var card = elements.create('card', {style: style});
+card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    // Prevent default form submission
+    ev.preventDefault();
+    // Disable card element and submit button to prevent multiple submissions
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    // Confirm payment
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        // Error handling
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            // Re-enable card element and submit button
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        // Payment success    
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
@@ -9,6 +13,10 @@ def checkout(request):
     Handle the checkout process for customer orders.
     Validates basket contents and displays order form.
     """
+    # Initialize Stripe
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
     # Get basket from session
     basket = request.session.get('basket', {})
 
@@ -16,14 +24,33 @@ def checkout(request):
     if not basket:
         messages.error(request, "Your basket is empty.")
         return redirect(reverse('products'))
+    
+    # Calculate total cost of basket
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+
+    # Create a Stripe payment intent
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     # Initialize the order form
     order_form = OrderForm()
+
+    # Check if stripe_public_key is available
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
 
     # Prepare template context
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
+    # Render the checkout template
     return render(request, template, context)
