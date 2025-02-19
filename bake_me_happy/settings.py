fd37f82415a697e05c88f8ff580a4ec5832fc36d
Sarah-Bue <buettner-sarah@gmx.net>
2025-02-19 12:41:30 +0000

    feat: add basic styling to allauth templates login, logout, signup, password_reset

--- bake_me_happy/settings.py
+++ bake_me_happy/settings.py
@@ -114,6 +114,8 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            # Custom allauth templates
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
