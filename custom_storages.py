4a4a52c91372c1f813e71eab42b94895e594a861
Sarah-Bue <buettner-sarah@gmx.net>
2025-02-25 12:15:29 +0000

    connect django to s3 for deployment

---
+++ custom_storages.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    """
+    Static Storage Class.
+    """
+    
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    """
+    Media Storage Class.
+    """
+
+    location = settings.MEDIAFILES_LOCATION
