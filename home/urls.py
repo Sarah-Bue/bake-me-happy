b9ae20329bc5d1f388295414641dabb7e4c828ef
Sarah-Bue <buettner-sarah@gmx.net>
2025-03-03 22:00:34 +0000

    feat: add store_management template for admin features

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Home Page
     path('', views.index, name='home'),
+
+    # Store Management Page
+    path('store_management/', views.store_management, name='store_management'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -7,3 +8,13 @@ def index(request):
     """
 
     return render(request, 'home/index.html')
+
+
+@login_required
+def store_management(request):
+    """
+    A view to return the store management dashboard.
+    Only accessible by admins.
+    """
+  
+    return render(request, 'home/store_management.html')
