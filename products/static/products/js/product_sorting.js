fecdb850497a99bad05b24227a2ddef9adbf6328
Sarah-Bue <buettner-sarah@gmx.net>
2025-03-22 15:07:29 +0000

    fix: update naming issues with ids in profile toggle. moved longer postload js sections into their own files

---
+++ products/static/products/js/product_sorting.js
@@ -0,0 +1,22 @@
+//The sorting functionality JS has been adapted from Code Institute's "Boutique Ado" project 
+
+$('#sort-selector').change(function() {
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.val();
+    if(selectedVal != "reset"){
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+
+        window.location.replace(currentUrl);
+    }
+})
